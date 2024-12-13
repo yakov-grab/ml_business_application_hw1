0c4cff873cefcaea27f6ea4ae8e3fa8542f8abdb
yakov-grab <yakov.grab@yandex.ru>
2024-12-13 14:56:36 +0300

    update plot.py

--- plot/src/plot.py
+++ plot/src/plot.py
@@ -2,23 +2,28 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import time
 import os
 
 log_file_path = './logs/metric_log.csv'
 
 while True:
-    if os .path.exists(log_file_path):
+    if os.path.exists(log_file_path):
         # Читаем данные из CSV файла
         data = pd.read_csv(log_file_path)
 
         # Проверяем, есть ли данные для построения графика
         if not data.empty:
             plt.figure(figsize=(10, 6))
-            plt.hist(data['absolute_error'], bins=30, color='blue', alpha=0.7)
-            plt.title('Распределение абсолютных ошибок')
-            plt.xlabel('Абсолютная ошибка')
-            plt.ylabel('Частота')
+
+            # Построение гистограммы и KDE
+            sns.histplot(data['absolute_error'], bins=10, color='orange', kde=True, stat="count", alpha=0.6)
+            sns.kdeplot(data['absolute_error'], color='orange', linewidth=2)
+
+            # Настройка осей и подписи
+            plt.xlabel('absolute_error')
+            plt.ylabel('Count')
             plt.grid(True)
 
             # Сохраняем график
